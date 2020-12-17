17bbcaf079c7292226cd7bb99be5a8866379d297
rabbykhairul <rabbykhairul@gmail.com>
2020-12-17 09:40:57 +0600

    respond with error when shortCode is not present on queryString to get ful url

--- src/api/short-url.js
+++ src/api/short-url.js
@@ -4,6 +4,7 @@ const validateReqToCreateShortUrl = require("../utils/validateReqToCreateShortUr
 const { getShortCode } = require("../services/shortUrlService");
 const router = express.Router();
 
+// build a short code for a long url
 router.post("/", validateReqToCreateShortUrl(), async (req, res) => {
   const { errors } = validationResult(req);
   if (errors.length > 0) return res.status(400).json({ errors: errors });
@@ -11,6 +12,19 @@ router.post("/", validateReqToCreateShortUrl(), async (req, res) => {
   const { originalURL } = req.body;
   const shortCode = await getShortCode(originalURL);
   res.json({ shortCode, originalURL });
+});
+
+// get the original url for a specific short code
+router.get("/", async (req, res) => {
+  const { shortCode } = req.query.shortCode;
+  if (!shortCode)
+    return res.status(400).json({
+      errors: {
+        msg: "Undefined queryString value for 'shortCode'",
+        param: "shortCode",
+        location: "query",
+      },
+    });
 });
 
 module.exports = router;
