1bf3a3b1d06e55786c5aaecf73b9fd02a68c77e0
rabbykhairul <rabbykhairul@gmail.com>
2020-12-17 09:24:24 +0600

    send back the generated shortCode with the original url in the response

--- src/api/short-url.js
+++ src/api/short-url.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
 const validateReqToCreateShortUrl = require("../utils/validateReqToCreateShortUrl");
+const { getShortCode } = require("../services/shortUrlService");
 const router = express.Router();
 
-router.post("/", validateReqToCreateShortUrl(), (req, res) => {
+router.post("/", validateReqToCreateShortUrl(), async (req, res) => {
   const { errors } = validationResult(req);
   if (errors.length > 0) return res.status(400).json({ errors: errors });
+
+  const { originalURL } = req.body;
+  const shortCode = await getShortCode(originalURL);
+  res.json({ shortCode, originalURL });
 });
 
 module.exports = router;
