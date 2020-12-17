b35d822043b7809f14ed871daac6f4679112428f
rabbykhairul <rabbykhairul@gmail.com>
2020-12-17 10:02:59 +0600

    decode a shortCode and send the original url in response

--- src/api/short-url.js
+++ src/api/short-url.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
 const validateReqToCreateShortUrl = require("../utils/validateReqToCreateShortUrl");
-const { getShortCode } = require("../services/shortUrlService");
+const { getShortCode, getOriginalURL } = require("../services/shortUrlService");
 const router = express.Router();
 
 // build a short code for a long url
@@ -16,15 +16,31 @@ router.post("/", validateReqToCreateShortUrl(), async (req, res) => {
 
 // get the original url for a specific short code
 router.get("/", async (req, res) => {
-  const { shortCode } = req.query.shortCode;
+  const { shortCode } = req.query;
   if (!shortCode)
     return res.status(400).json({
-      errors: {
-        msg: "Undefined queryString value for 'shortCode'",
+      errors: [
+        {
+          msg: "Undefined queryString value for 'shortCode'",
+          param: "shortCode",
+          location: "query",
+        },
+      ],
+    });
+
+  const originalURL = await getOriginalURL(shortCode);
+  if (originalURL) return res.json({ shortCode, originalURL });
+
+  res.status(400).json({
+    errors: [
+      {
+        value: shortCode,
+        msg: "Invalid short code - no original url exist for this short code",
         param: "shortCode",
         location: "query",
       },
-    });
+    ],
+  });
 });
 
 module.exports = router;
